8e46442af04f6d64cfec55e8f2847b89121b052a
haojt <jiangtao.hao@supwisdom.com>
2020-12-15 16:43:20 +0800

    init

---
+++ src/main/java/com/lhc/beihe/BeiheApplication.java
@@ -0,0 +1,13 @@
+package com.lhc.beihe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeiheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhc/beihe/BeiheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhc.beihe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeiheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
